02f27fe12796a2c2667255e8e700a29c4ff1bc15
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-03-01 08:05:46 +0000

    Initialize project

---
+++ src/main/java/com/testCorp/employeeDir/EmployeeDirApplication.java
@@ -0,0 +1,13 @@
+package com.testCorp.employeeDir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeDirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeDirApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testCorp/employeeDir/EmployeeDirApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testCorp.employeeDir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeDirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
