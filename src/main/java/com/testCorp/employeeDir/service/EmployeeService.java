0588ca364281fc82b01f7454bb4472d7380687a2
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-03-05 06:10:56 +0000

    Add findById, save and delete employee service methods

--- src/main/java/com/testCorp/employeeDir/service/EmployeeService.java
+++ src/main/java/com/testCorp/employeeDir/service/EmployeeService.java
@@ -7,4 +7,10 @@ import java.util.List;
 public interface EmployeeService {
 
     public List<Employee> findAll();
+
+    public Employee findById(Integer id);
+
+    public Employee save(Employee employee);
+
+    public void deleteById(Integer id);
 }
