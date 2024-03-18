048294a97af9acf0032287ed1578f79dc219e291
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-03-18 06:11:50 +0000

    Switch from DAOs to JPA repository

--- src/main/java/com/testCorp/employeeDir/service/EmployeeService.java
+++ src/main/java/com/testCorp/employeeDir/service/EmployeeService.java
@@ -3,12 +3,13 @@ package com.testCorp.employeeDir.service;
 import com.testCorp.employeeDir.entity.Employee;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface EmployeeService {
 
     public List<Employee> findAll();
 
-    public Employee findById(Integer id);
+    public Optional<Employee> findById(Integer id);
 
     public Employee save(Employee employee);
 
--- src/main/java/com/testCorp/employeeDir/service/EmployeeServiceImp.java
+++ src/main/java/com/testCorp/employeeDir/service/EmployeeServiceImp.java
@@ -1,42 +1,43 @@
 package com.testCorp.employeeDir.service;
 
-import com.testCorp.employeeDir.dao.EmployeeDAO;
 import com.testCorp.employeeDir.entity.Employee;
+import com.testCorp.employeeDir.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EmployeeServiceImp implements EmployeeService{
 
-    private final EmployeeDAO employeeDAO;
+    private final EmployeeRepository employeeRepository;
 
     @Autowired
-    public EmployeeServiceImp(EmployeeDAO employeeDAO){
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImp(EmployeeRepository employeeRepository){
+        this.employeeRepository = employeeRepository;
     }
 
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
-    public Employee findById(Integer id) {
-        return employeeDAO.findById(id);
+    public Optional<Employee> findById(Integer id) {
+        return employeeRepository.findById(id);
     }
 
     @Transactional
     @Override
     public Employee save(Employee employee) {
-        return employeeDAO.save(employee);
+        return employeeRepository.save(employee);
     }
 
     @Transactional
     @Override
     public void deleteById(Integer id) {
-        employeeDAO.deleteById(id);
+        employeeRepository.deleteById(id);
     }
 }
