b97697f9fa61d348a209e1659a23016b99f02eb0
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-03-18 05:19:31 +0000

    Add apis to update and delete a given employee

--- src/main/java/com/testCorp/employeeDir/rest/EmployeeRestController.java
+++ src/main/java/com/testCorp/employeeDir/rest/EmployeeRestController.java
@@ -5,6 +5,7 @@ import com.testCorp.employeeDir.service.EmployeeService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -33,5 +34,23 @@ public class EmployeeRestController {
     @PostMapping("")
     public Employee addEmployee(@RequestBody Employee employee) {
         return employeeService.save(employee);
+    }
+
+    @PutMapping("")
+    public Employee updateEmployee(@RequestBody Employee employee) throws Exception {
+        if(employee.getId() == 0){
+            throw new Exception("Employee does not exist");
+        }
+        Employee foundEmployee = employeeService.findById(employee.getId());
+        if(foundEmployee == null){
+            throw new Exception("Employee with id=" + employee.getId() + " does not exist");
+        }
+        return employeeService.save(employee);
+    }
+
+    @DeleteMapping("/{employeeId}")
+    public String deleteEmployee(@PathVariable Integer employeeId){
+        employeeService.deleteById(employeeId);
+        return "Successfully deleted employee with id=" + employeeId;
     }
 }
