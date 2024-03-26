34a79c58d3afefe2db4fd529792057c4cddae614
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-03-26 05:55:28 +0000

    Alter base path for employee rest apis

--- src/main/java/com/testCorp/employeeDir/repository/EmployeeRepository.java
+++ src/main/java/com/testCorp/employeeDir/repository/EmployeeRepository.java
@@ -2,6 +2,13 @@ package com.testCorp.employeeDir.repository;
 
 import com.testCorp.employeeDir.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+/*
+    The annotation below is used to alter the base path for the entity
+    By default the path is the entity name with an 's' attached to it.
+
+*/
+@RepositoryRestResource(path="employees")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 }
