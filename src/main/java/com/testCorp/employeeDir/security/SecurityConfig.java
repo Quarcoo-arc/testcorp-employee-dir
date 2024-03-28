38cbb59dd40e23f0c23c81b24269c1c858138b50
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-03-28 22:00:54 +0000

    Set up access restrictions on api endpoints

--- src/main/java/com/testCorp/employeeDir/security/SecurityConfig.java
+++ src/main/java/com/testCorp/employeeDir/security/SecurityConfig.java
@@ -2,9 +2,14 @@ package com.testCorp.employeeDir.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
@@ -30,6 +35,25 @@ public class SecurityConfig {
                 .build();
 
         return new InMemoryUserDetailsManager(john, mary, susan);
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain (HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(configure -> configure
+                .requestMatchers(HttpMethod.GET, "/api/employees").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.GET, "/api/employees/**").hasRole("EMPLOYEE")
+                .requestMatchers(HttpMethod.POST, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.PUT, "/api/employees").hasRole("MANAGER")
+                .requestMatchers(HttpMethod.DELETE, "/api/employees/**").hasRole("ADMIN")
+        );
+
+//        use basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+//        disable CSRF - not required for stateless REST apis
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 
 }
